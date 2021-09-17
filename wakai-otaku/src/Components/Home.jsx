53927ac1cf3081c157b6ff2cbddd59bad9eb7e9f
JosueADiazz <japaricio@solucionesroots.com>
2021-09-17 16:08:52 -0600

    chatbot

--- wakai-otaku/src/Components/Home.jsx
+++ wakai-otaku/src/Components/Home.jsx
@@ -1,15 +1,19 @@
-import React, { useEffect } from 'react';
-import { Slider } from './Slider/Slider';
-import { MasVistos } from './Animes/MasVistos';
-import { MasPopulares } from './Animes/MasPopulares';
+import React, { useEffect } from "react";
+import { Slider } from "./Slider/Slider";
+import { MasVistos } from "./Animes/MasVistos";
+import { MasPopulares } from "./Animes/MasPopulares";
 
 export const Home = () => {
-
-    return (
-        <>
-            
-            <Slider title="ANTENKIU ANIME - El Rincon del Otaku"></Slider>
-            <MasPopulares></MasPopulares>
-        </>
-    )
-}
+  return (
+    <>
+      <Slider title="ANTENKIU ANIME - El Rincon del Otaku"></Slider>
+      <MasPopulares></MasPopulares>
+      <df-messenger
+        intent="WELCOME"
+        chat-title="anteiku"
+        agent-id="06177e9b-3670-4c9d-8c68-6988a7004e25"
+        language-code="es"
+      ></df-messenger>
+    </>
+  );
+};
